6b7991d4ea360b87d0f7a2635576bf0cadff9db8
ABIOYE MARVELOUS OLAITAN ENOCH <marvelousenoch160@gmail.com>
2026-08-13 12:40:31 -0500

    Add complete schema and documentation for MarvelPay

--- database/schema.sql
+++ database/schema.sql
@@ -3,6 +3,9 @@
 -- Author: ABIOYE MARVELOUS OLAITAN ENOCH (CEO, Marvel Technologies)
 -- =====================================================================
 
+-- BUSINESS RULE: Transaction history cannot be updated, modifed or deleted.
+--
+
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(56) UNIQUE NOT NULL,
@@ -18,3 +21,10 @@ CREATE TABLE wallets (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    wallet_id INT NOT NULL REFERENCES wallets(wallet_id),
+    amount NUMERIC(12, 2) NOT NULL CHECK (amount > 0.00),
+    transaction_type TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
